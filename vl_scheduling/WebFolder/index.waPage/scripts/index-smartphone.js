cf1f397fd7c3277d2559c8fc4c904a86f12f6e34
ssmitty65 <sean@vocalinks.com>
2014-05-12 11:58:46 -0400

    Update #12
    add tamara to directory

--- vl_scheduling/WebFolder/index.waPage/scripts/index-smartphone.js
+++ vl_scheduling/WebFolder/index.waPage/scripts/index-smartphone.js
@@ -2,17 +2,10 @@
 WAF.onAfterInit = function onAfterInit() {// @lock
 
 // @region namespaceDeclaration// @startlock
-	var login2 = {};	// @login
 // @endregion// @endlock
 
 // eventHandlers// @lock
 
-	login2.login = function login2_login (event)// @startlock
-	{// @endlock
-		alert("Hello");
-	};// @lock
-
 // @region eventManager// @startlock
-	WAF.addListener("login2", "login", login2.login, "WAF");
 // @endregion
 };// @endlock
