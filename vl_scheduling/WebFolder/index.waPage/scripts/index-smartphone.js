74fd5d535707ac27f8d355611d5df16de17c7ebd
ssmitty65 <sean@vocalinks.com>
2014-05-12 11:34:48 -0400

    Update #10
    login test

---
+++ vl_scheduling/WebFolder/index.waPage/scripts/index-smartphone.js
@@ -0,0 +1,18 @@
+﻿
+WAF.onAfterInit = function onAfterInit() {// @lock
+
+// @region namespaceDeclaration// @startlock
+	var login2 = {};	// @login
+// @endregion// @endlock
+
+// eventHandlers// @lock
+
+	login2.login = function login2_login (event)// @startlock
+	{// @endlock
+		alert("Hello");
+	};// @lock
+
+// @region eventManager// @startlock
+	WAF.addListener("login2", "login", login2.login, "WAF");
+// @endregion
+};// @endlock
