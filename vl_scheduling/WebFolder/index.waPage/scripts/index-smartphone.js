516ebf284576296f4041ca9a5cc40c3e5bc2028b
Waka-Mini <waka@vocalinks.com>
2014-05-12 15:39:00 -0400

    Update #14
    widgit tested - ok

--- vl_scheduling/WebFolder/index.waPage/scripts/index-smartphone.js
+++ vl_scheduling/WebFolder/index.waPage/scripts/index-smartphone.js
@@ -2,10 +2,18 @@
 WAF.onAfterInit = function onAfterInit() {// @lock
 
 // @region namespaceDeclaration// @startlock
+	var login1 = {};	// @login
 // @endregion// @endlock
 
 // eventHandlers// @lock
 
+	login1.login = function login1_login (event)// @startlock
+	{// @endlock
+		//alert("Successfully Connected");
+		$$('navigationView1').show();
+	};// @lock
+
 // @region eventManager// @startlock
+	WAF.addListener("login1", "login", login1.login, "WAF");
 // @endregion
 };// @endlock
