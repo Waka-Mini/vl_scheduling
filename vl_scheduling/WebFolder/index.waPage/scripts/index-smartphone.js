1acca6d6a24f5e49c2d5bdbc05ce25a01cf378f2
Waka-Mini <waka@vocalinks.com>
2014-05-13 09:47:21 -0400

    Update #16
    waka server - datastore testing

--- vl_scheduling/WebFolder/index.waPage/scripts/index-smartphone.js
+++ vl_scheduling/WebFolder/index.waPage/scripts/index-smartphone.js
@@ -2,19 +2,10 @@
 WAF.onAfterInit = function onAfterInit() {// @lock
 
 // @region namespaceDeclaration// @startlock
-	var button2 = {};	// @button
 	var login1 = {};	// @login
 // @endregion// @endlock
 
 // eventHandlers// @lock
-
-	button2.click = function button2_click (event)// @startlock
-	{// @endlock
-		 sources.vl4d.DoubleSalary({ 
-            onSuccess:function(event){
-                //sources.employees.allEntities();
-            }});
-	};// @lock
 
 	login1.login = function login1_login (event)// @startlock
 	{// @endlock
@@ -23,7 +14,6 @@ WAF.onAfterInit = function onAfterInit() {// @lock
 	};// @lock
 
 // @region eventManager// @startlock
-	WAF.addListener("button2", "click", button2.click, "WAF");
 	WAF.addListener("login1", "login", login1.login, "WAF");
 // @endregion
 };// @endlock
